35f9d87ff2b72adbd7e5787cb0e325f533047899
Kunal Keshan <kunalkeshan12@gmail.com>
2022-01-18 19:37:34 +0530

    📂 MERN File Structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import { ErrorBoundary } from "react-error-boundary";
 import AppRoutes from "./components/AppRoutes/AppRoutes";
 import Error from "./components/Error/Error";
-import Logo from "./assets/logo.svg"
+import Logo from "./assets/logo/logo.svg"
 
 function App() {
   return (
     <div className="App">
       <ErrorBoundary FallbackComponent={<Error />} onError={(error) => console.log(error)}>
         <AppRoutes />
-        <img src={Logo} alt="Sundar Clinic Logo"/>
+        <img src={Logo} alt="Sundar Clinic Logo" />
       </ErrorBoundary>
     </div>
   );
